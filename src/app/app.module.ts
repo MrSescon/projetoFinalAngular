34ab4f9e8652a5fb89ddfaf0fb3491bd01743694
Magno Sescon <magnosescon@gmail.com>
2020-01-05 11:10:35 -0300

    Adicionado o módulo Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing-module';
+import { DashboardModule } from './dashboard';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    DashboardModule,
     AppRoutingModule
   ],
   providers: [],
